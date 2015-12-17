9cbd9592774c8f87bcef542b687b4d014fb9a50b
Richard Turek <rjt1357@gmail.com>
2015-12-17 18:05:30 -0500

    restructure

--- manageaws/ec2_instance.py
+++ manageaws/ec2_instance.py
@@ -1,10 +1,31 @@
 # boto3 brings back a dict, so json processing is not needed
 import boto3
-import pprint
+from pprint import pprint
 
-ec2 = boto3.resource('ec2')
+ec2_client = boto3.client('ec2')
 
-for status in ec2.meta.client.describe_instance_status()['InstanceStatuses']:
-    pprint.pprint(status)
-    print('instanceId ' + status['InstanceId'])
+reservations = ec2_client.describe_instances().get('Reservations')
+# pprint(reservations)
+
+def get_instances_matching_tag(partial_tag):
+    print('Looking for EC2 instances with "' + partial_tag + '" in one of the tags')
+    matching_instances = []
+    for res in reservations:
+        all_instances = res.get('Instances')
+        for inst in all_instances:
+            tags = inst.get('Tags')
+            for tag in tags:
+                if partial_tag in tag.get('Value'):
+                    matching_instances.append(inst)
+                    break
+    return matching_instances
+
+
+
+def main():
+    matching_instances = get_instances_matching_tag('rjt')
+    print(len(matching_instances))
+
+if __name__ == "__main__":
+    main()
 
